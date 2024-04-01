5c2fe568da8aa88e3fda2a7617db86edc4115f0f
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-04-01 21:05:17 +0300

    removed provider

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,27 +1,27 @@
-// context/AuthContext.tsx
-import React, { createContext, useContext, useEffect } from 'react';
-import { useAuthStore } from '../store/session.store';
-import supabase from '@/utils/supabaseClient';
+// // context/AuthContext.tsx
+// import React, { createContext, useContext, useEffect } from 'react';
+// import { useAuthStore } from '../store/session.store';
+// import supabase from '@/utils/supabaseClient';
 
-const AuthContext = createContext({});
+// const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const setSession = useAuthStore((state) => state.setSession);
+// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//     const setSession = useAuthStore((state) => state.setSession);
 
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session)
-        })
+//     useEffect(() => {
+//         supabase.auth.getSession().then(({ data: { session } }) => {
+//             setSession(session)
+//         })
 
-        const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
+//         const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+//             setSession(session);
+//         });
 
-        return () => subscription?.subscription.unsubscribe()
+//         return () => subscription?.subscription.unsubscribe()
         
-    }, [setSession]);
+//     }, [setSession]);
 
-    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
-};
+//     return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+// };
 
-export const useAuth = () => useContext(AuthContext);
+// export const useAuth = () => useContext(AuthContext);
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,7 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import supabase from "@/utils/supabaseClient";
-import { AuthProvider } from "@/contexts/AuthContext";
+// import { AuthProvider } from "@/contexts/AuthContext";
 import { useAuthStore } from "@/store/session.store";
 
 
@@ -15,31 +15,29 @@ export const Route = createRootRoute({
         const session = useAuthStore(state => state.session);
 
         return (
-            <AuthProvider>
-                <ThemeHandler defaultTheme="dark" storageKey="vite-ui-theme">
-                    {
-                        !session
-                            ?
-                            <div className="h-screen flex justify-center items-center">
-                                <div className="space-y-4">
-                                    <h1 className="text-center text-2xl font-bold">Smart TODO</h1>
-                                    <Auth supabaseClient={supabase} providers={["google", "github"]} socialLayout="horizontal" appearance={{ theme: ThemeSupa }} />
-                                </div>
+            <ThemeHandler defaultTheme="dark" storageKey="vite-ui-theme">
+                {
+                    !session
+                        ?
+                        <div className="h-screen flex justify-center items-center">
+                            <div className="space-y-4">
+                                <h1 className="text-center text-2xl font-bold">Smart TODO</h1>
+                                <Auth supabaseClient={supabase} providers={["google", "github"]} socialLayout="horizontal" appearance={{ theme: ThemeSupa }} />
                             </div>
-                            :
-                            <>
-                                <div className="p-3">
-                                    <Navbar />
-                                </div>
-                                <main className="px-[1%]">
-                                    <Outlet />
-                                </main>
-                                <TanStackRouterDevtools />
-                            </>
+                        </div>
+                        :
+                        <>
+                            <div className="p-3">
+                                <Navbar />
+                            </div>
+                            <main className="px-[1%]">
+                                <Outlet />
+                            </main>
+                            <TanStackRouterDevtools />
+                        </>
 
-                    }
-                </ThemeHandler>
-            </AuthProvider>
+                }
+            </ThemeHandler>
         );
     },
 })
