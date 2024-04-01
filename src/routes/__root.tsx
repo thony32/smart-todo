5b524da5d1d60ea5ffa12e044a8d5094c531276e
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-04-01 20:40:55 +0300

    auth store and provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,55 +3,43 @@ import ThemeHandler from "../components/ThemeHandler";
 import Navbar from "../components/Navbar";
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
-
-import { Session } from '@supabase/supabase-js'
-import { useEffect, useState } from "react";
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import supabase from "@/utils/supabaseClient";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { useAuthStore } from "@/store/session.store";
+
 
 export const Route = createRootRoute({
     component: () => {
-        const [session, setSession] = useState<Session | null>(null)
-
-        useEffect(() => {
-            supabase.auth.getSession().then(({ data: { session } }) => {
-                setSession(session)
-            })
-
-            const {
-                data: { subscription },
-            } = supabase.auth.onAuthStateChange((_event, session) => {
-                setSession(session)
-            })
-
-            return () => subscription.unsubscribe()
-        }, [])
+        const session = useAuthStore(state => state.session);
 
         return (
-            <ThemeHandler defaultTheme="dark" storageKey="vite-ui-theme">
-                {
-                    !session
-                        ?
-                        <div className="h-screen flex justify-center items-center">
-                            <div className="space-y-4">
-                                <h1 className="text-center text-2xl font-bold">Smart TODO</h1>
-                                <Auth supabaseClient={supabase} providers={["google", "github"]} socialLayout="horizontal" appearance={{ theme: ThemeSupa }} />
+            <AuthProvider>
+                <ThemeHandler defaultTheme="dark" storageKey="vite-ui-theme">
+                    {
+                        !session
+                            ?
+                            <div className="h-screen flex justify-center items-center">
+                                <div className="space-y-4">
+                                    <h1 className="text-center text-2xl font-bold">Smart TODO</h1>
+                                    <Auth supabaseClient={supabase} providers={["google", "github"]} socialLayout="horizontal" appearance={{ theme: ThemeSupa }} />
+                                </div>
                             </div>
-                        </div>
-                        :
-                        <>
-                            <div className="p-3">
-                                <Navbar />
-                            </div>
-                            <main className="px-[1%]">
-                                <Outlet />
-                            </main>
-                            <TanStackRouterDevtools />
-                        </>
+                            :
+                            <>
+                                <div className="p-3">
+                                    <Navbar />
+                                </div>
+                                <main className="px-[1%]">
+                                    <Outlet />
+                                </main>
+                                <TanStackRouterDevtools />
+                            </>
 
-                }
-            </ThemeHandler>
+                    }
+                </ThemeHandler>
+            </AuthProvider>
         );
     },
 })
---
+++ src/store/session.store.ts
@@ -0,0 +1,13 @@
+// store/useAuthStore.ts
+import { create } from "zustand"
+import { Session } from "@supabase/supabase-js"
+
+interface AuthState {
+    session: Session | null
+    setSession: (session: Session | null) => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+    session: null,
+    setSession: (session) => set({ session }),
+}))
