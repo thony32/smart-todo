d2af8fb8c072f2d02019dab9934c867242922cfc
tiavinacyri <tiavinacyri@gmail.com>
2024-04-03 12:57:28 +0400

    use formik

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,27 +3,37 @@ import { useState } from "react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+});
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
     const [isLogin, setIsLogin] = useState(false);
 
-    const handleLogin = async (event: any) => {
-        event.preventDefault();
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: async (values) => {
+            await setIsLogin(true);
 
-        await setIsLogin(true);
-        const { error } = await supabase.auth.signInWithPassword({
-            email,
-            password,
-        });
-        await setIsLogin(false);
-
-        if (error) {
-            console.error(error);
+            const { error } = await supabase.auth.signInWithPassword({
+                email: values.email,
+                password: values.password,
+            });
+            if (error) {
+                console.error(error);
+            }
+            
+            await setIsLogin(false);
         }
-    };
+    });
 
     const handleSocialLogin = async (provider: any) => {
         const { error } = await supabase.auth.signInWithOAuth({
@@ -39,15 +49,25 @@ const Login = () => {
                 <h1>This is the custom login for smart TODO</h1>
             </div>
             <div>
-                <form className="space-y-5" onSubmit={handleLogin}>
+                <form className="space-y-5" onSubmit={formik.handleSubmit}>
                     <div className="space-y-3">
                         <div className="grid w-full max-w-sm items-center gap-1.5">
-                            <Label htmlFor="email">Email</Label>
-                            <Input type="email" id="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} />
+                            <div className="flex justify-between">
+                                <Label htmlFor="email">Email</Label>
+                                {formik.touched.email && formik.errors.email && (
+                                    <div className="text-xs text-error">{formik.errors.email}</div>
+                                )}
+                            </div>
+                            <Input type="email" id="email" placeholder="Email" {...formik.getFieldProps('email')} />
                         </div>
                         <div className="grid w-full max-w-sm items-center gap-1.5">
-                            <Label htmlFor="password">Password</Label>
-                            <Input type="password" id="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                            <div className="flex justify-between">
+                                <Label htmlFor="password">Password</Label>
+                                {formik.touched.password && formik.errors.password && (
+                                    <div className="text-xs text-error">{formik.errors.password}</div>
+                                )}
+                            </div>
+                            <Input type="password" id="password" placeholder="Password" {...formik.getFieldProps('password')} />
                         </div>
                         <div className="flex justify-end">
                             <Button className="flex items-center gap-2" type="submit">
