6536a2b2f309915fb2c6a33647f66af11a6750f0
tiavinacyri <tiavinacyri@gmail.com>
2024-04-08 12:25:44 +0400

    update todo list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,9 +5,10 @@ import { useEffect, useRef } from "react"
 import toast from "react-hot-toast"
 import { Input } from "./ui/input"
 import useKeyboard from "@/utils/useKeyboard"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import Todo from "@/models/Todo"
 import { Link } from "@tanstack/react-router"
+import formatDate from "@/utils/dateFormat"
 
 const getTodos = async () => {
     return TodoService.getTodos()
@@ -50,7 +51,7 @@ const TaskList = () => {
 
     return (
         <div className="space-y-6">
-            <div className="flex justify-between items-center px-[10%]">
+            <div className="flex justify-between items-center px-[2%]">
                 <div>
                     <h1 className="font-semibold first-letter:text-2xl">List of your todo</h1>
                 </div>
@@ -67,41 +68,37 @@ const TaskList = () => {
                 </div>
             </div>
             {/* List of todo */}
-            <div className="grid grid-cols-4 gap-5">
-                {todos?.map((todo: Todo) => (
-                    <Link key={todo.id} to="/taskItems">
-                        <Card className="cursor-pointer hover:scale-105 duration-300">
-                            <CardHeader>
-                                <CardTitle>{todo.name}</CardTitle>
-                                <CardDescription>{todo.description}</CardDescription>
-                            </CardHeader>
-                            {/* aperçu anle task fotsn f mila clickena de ao hita dool ny fandeany */}
-                            <CardContent>
-                                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                                    <span className="flex h-2 w-2 translate-y-1 rounded-full bg-green-500" />
-                                    <div className="space-y-1">
+            <div className="h-[80dvh] overflow-y-auto">
+                <div className="grid grid-cols-5 gap-4 px-[2%]">
+                    {todos?.map((todo: Todo) => (
+                        <Link key={todo.id} to="/taskItems">
+                            <Card className="cursor-pointer relative h-52 group hover:shadow-[0px_9px_10px_-3px] hover:shadow-success duration-100">
+                                <CardHeader>
+                                    <CardTitle className="group-hover:-translate-y-2 group-hover:text-success capitalize duration-75">{todo.name}</CardTitle>
+                                    <CardDescription className="truncate">{todo.description}</CardDescription>
+                                </CardHeader>
+                                {/* aperçu anle task fotsn f mila clickena de ao hita dool ny fandeany */}
+                                <CardContent>
+                                    <div className="mb-2 grid grid-cols-[25px_1fr] items-start pb-2 last:mb-0 last:pb-0">
+                                        <span className="flex h-2 w-2 translate-y-1 rounded-full bg-green-500" />
                                         <p className="text-sm font-medium leading-none">Task one</p>
-                                        <p className="text-sm text-muted-foreground">Desciption one</p>
                                     </div>
-                                </div>
-                                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                                    <span className="flex h-2 w-2 translate-y-1 rounded-full bg-gray-500" />
-                                    <div className="space-y-1">
+                                    <div className="mb-2 grid grid-cols-[25px_1fr] items-start pb-2 last:mb-0 last:pb-0">
+                                        <span className="flex h-2 w-2 translate-y-1 rounded-full bg-gray-500" />
                                         <p className="text-sm font-medium leading-none">Task one</p>
-                                        <p className="text-sm text-muted-foreground">Desciption one</p>
                                     </div>
-                                </div>
-                                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                                    <span className="flex h-2 w-2 translate-y-1 rounded-full bg-yellow-500" />
-                                    <div className="space-y-1">
+                                    <div className="mb-2 grid grid-cols-[25px_1fr] items-start pb-2 last:mb-0 last:pb-0">
+                                        <span className="flex h-2 w-2 translate-y-1 rounded-full bg-yellow-500" />
                                         <p className="text-sm font-medium leading-none">Task one</p>
-                                        <p className="text-sm text-muted-foreground">Desciption one</p>
                                     </div>
+                                </CardContent>
+                                <div className="absolute right-3 bottom-2">
+                                    <p className="text-xs">{formatDate(todo.created_at)}</p>
                                 </div>
-                            </CardContent>
-                        </Card>
-                    </Link>
-                ))}
+                            </Card>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
