ff8356118e9bd5fbf123ac96377f165cd477c0fb
tiavinacyri <tiavinacyri@gmail.com>
2024-04-12 13:00:32 +0400

    give an numerotation to task

--- src/services/GeminiService.ts
+++ src/services/GeminiService.ts
@@ -1,26 +1,15 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
-<<<<<<< HEAD
 interface TodoItems {
     todos: {
         number: number;
         todo: string;
     }[]
 }
-
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY as string;
-
 class GeminiService {
     async getMerlinData({ todos }: TodoItems) {
-        const message = "Prioritize the following tasks based on reality: \n" + JSON.stringify(todos) + '\n' + 'just give the array rearrange in response';
-=======
-const GEMINI_API_KEY = 'AIzaSyDqGNaavyeDZWtVdHx2fOO_YXE0CJ9_cAY'
-
-class GeminiService {
-    async getMerlinData(todos: any[]) {
-        const todosJoin = todos.map(todo => todo.todo).join(",");
-        const message = "Prioritize the following tasks based on reality: " + todosJoin;
->>>>>>> init gemini
+        const todosJoin = todos.map(todo => todo.number + ': ' + todo.todo).join("\n");
+        const message = "Prioritize the following tasks based on reality: \n" + todosJoin + "\n" + "Just give the number of the task without separator in the order you want them to be done.";
 
         const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
@@ -28,13 +17,9 @@ class GeminiService {
         const result = await model.generateContent(message);
         const response = await result.response;
 
-<<<<<<< HEAD
         const data = response?.candidates?.[0]?.content?.parts?.[0]?.text as string;
 
         return JSON.parse(data) || [];
-=======
-        return response || [];
->>>>>>> init gemini
     }
 }
 
