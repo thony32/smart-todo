60c934157c7d1db7fcb4cd61ac1cbcc11a389276
tiavinacyri <tiavinacyri@gmail.com>
2024-04-12 10:19:37 +0400

    mail notifier icon

--- src/components/UserInfos.tsx
+++ src/components/UserInfos.tsx
@@ -22,12 +22,14 @@ const UserInfos = () => {
             toast("Your will now get notification by mail", {
                 position: "bottom-right",
                 className: "text-sm",
+                icon: '📬'
             })
         } else {
             await NotificationService.removeNotification(session.user.id);
             toast("Your will not get notification by mail", {
                 position: "bottom-right",
                 className: "text-sm",
+                icon: '📭'
             })
         }
     }
@@ -66,8 +68,8 @@ const UserInfos = () => {
         <div className="user-infos">
             {avatar_url && (
                 <div className='flex gap-4 items-center relative'>
-                    <div className='absolute -top-1 -left-4 sm:-left-6 tooltip tooltip-bottom tooltip-success' data-tip='Get mail notify'>
-                        <svg className={`w-5 cursor-pointer hover:rotate-12 duration-200 ${isGetMailNotify && 'stroke-success scale-110'}`} onClick={getMailNotify} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <div onClick={getMailNotify} className='absolute -top-1 -left-4 sm:-left-6 tooltip tooltip-bottom cursor-pointer' data-tip='Mail notifier'>
+                        <svg className={`w-5 cursor-pointer hover:rotate-12 duration-200 ${isGetMailNotify && 'stroke-success scale-110'}`} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                         </svg>
                     </div>
