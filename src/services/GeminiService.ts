c014a5284ec8e621ff58020655f09600e2a8d158
tiavinacyri <tiavinacyri@gmail.com>
2024-04-17 09:17:23 +0400

    rebase 2.0

--- src/services/GeminiService.ts
+++ src/services/GeminiService.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+<<<<<<< HEAD
 interface TodoItems {
     todos: {
         number: number;
@@ -12,6 +13,14 @@ const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY as string;
 class GeminiService {
     async getMerlinData({ todos }: TodoItems) {
         const message = "Prioritize the following tasks based on reality: \n" + JSON.stringify(todos) + '\n' + 'just give the array rearrange in response';
+=======
+const GEMINI_API_KEY = 'AIzaSyDqGNaavyeDZWtVdHx2fOO_YXE0CJ9_cAY'
+
+class GeminiService {
+    async getMerlinData(todos: any[]) {
+        const todosJoin = todos.map(todo => todo.todo).join(",");
+        const message = "Prioritize the following tasks based on reality: " + todosJoin;
+>>>>>>> init gemini
 
         const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
@@ -19,9 +28,13 @@ class GeminiService {
         const result = await model.generateContent(message);
         const response = await result.response;
 
+<<<<<<< HEAD
         const data = response?.candidates?.[0]?.content?.parts?.[0]?.text as string;
 
         return JSON.parse(data) || [];
+=======
+        return response || [];
+>>>>>>> init gemini
     }
 }
 
