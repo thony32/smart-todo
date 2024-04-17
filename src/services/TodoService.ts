d3125c47f89d1d3da5f3a68e91b4579d7e600eb3
tiavinacyri <tiavinacyri@gmail.com>
2024-04-17 09:23:27 +0400

    clean todoService

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -2,17 +2,6 @@ import supabase from "@/utils/supabaseClient";
 import Todo from "@/models/Todo";
 
 class TodoService {
-    async getTodos({ front_user_id, search = '', page_size = 15 }: { front_user_id: string | undefined; search?: string; page_size?: number; page_number?: number }): Promise<Todo[]> {
-        try {
-            const { data: Todos, error } = await supabase.rpc('get_todos', { front_user_id, search, page_size });
-            if (error) {
-                throw error;
-            }
-            return Todos || [];
-        } catch (error) {
-            throw error;
-        }
-    }
     async getTodosItems(front_user_id: number, page_size: number, search: string = '') {
         try {
             const { data, error } = await supabase
